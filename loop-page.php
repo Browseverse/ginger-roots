00f3588719306012dec6a963687674cc4bb0d1e5
Yoav <yoav@gingerlime.com>
2012-01-08 22:42:54 +0100

    converted loop-page to mustache

--- loop-page.php
+++ loop-page.php
@@ -1,10 +1,18 @@
 <?php /* Start loop */ ?>
-<?php while (have_posts()) : the_post(); ?>
-  <?php roots_post_before(); ?>
-    <?php roots_post_inside_before(); ?>
-      <h1><?php the_title(); ?></h1>
-      <?php the_content(); ?>
-      <?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>' )); ?>
-    <?php roots_post_inside_after(); ?>
-  <?php roots_post_after(); ?>
-<?php endwhile; // End the loop ?>
+<?php //while (have_posts()) : the_post(); 
+
+//include_once('Mustache.php');
+/* template is loaded from the same filename, with extension .mustache */
+$template = file_get_contents(dirname(__FILE__) . '/' . basename(__FILE__, '.php').'.mustache');
+
+
+$context = new Rootache;
+/* Here you can add application logic, function calls or whatever you need to
+ * customize your template. Then add these to the context, e.g.
+ * $context->variable_name = "value"; */
+
+//$m = new Mustache;
+echo $context->render($template);
+//echo the_content();
+
+//endwhile; // End the loop ?>
