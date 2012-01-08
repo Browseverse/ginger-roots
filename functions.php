4893c90268af5cd06bb9de413667170543183edb
Yoav <yoav@gingerlime.com>
2012-01-08 22:42:22 +0100

    moved roots_entry_meta into a partial template

--- functions.php
+++ functions.php
@@ -98,12 +98,6 @@ function roots_register_sidebars() {
 
 add_action('widgets_init', 'roots_register_sidebars');
 
-// return post entry meta information
-function roots_entry_meta() {
-  echo '<time class="updated" datetime="'. get_the_time('c') .'" pubdate>'. sprintf(__('Posted on %s at %s.', 'roots'), get_the_time('l, F jS, Y'), get_the_time()) .'</time>';
-  echo '<p class="byline author vcard">'. __('Written by', 'roots') .' <a href="'. get_author_posts_url(get_the_author_meta('id')) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
-}
-
 require_once('Mustache.php');
 require_once('MustacheLoader.php');
 
