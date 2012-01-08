9d4a4f78417771c70ee4b803f2b52be0314bd174
Yoav <yoav@gingerlime.com>
2012-01-08 22:43:22 +0100

    converted loop-single to mustache

--- loop-single.php
+++ loop-single.php
@@ -1,21 +1,16 @@
 <?php /* Start loop */ ?>
-<?php while (have_posts()) : the_post(); ?>
-  <?php roots_post_before(); ?>
-    <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-    <?php roots_post_inside_before(); ?>
-      <header>
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-        <?php roots_entry_meta(); ?>
-      </header>
-      <div class="entry-content">
-        <?php the_content(); ?>
-      </div>
-      <footer>
-        <?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>' )); ?>
-        <p><?php the_tags(); ?></p>
-      </footer>
-      <?php comments_template(); ?>
-      <?php roots_post_inside_after(); ?>
-    </article>
-  <?php roots_post_after(); ?>
-<?php endwhile; // End the loop ?>
+<?php
+$template = file_get_contents(dirname(__FILE__) . '/' . basename(__FILE__, '.php').'.mustache');
+$context = new Rootache;
+
+/* getting comments */
+ob_start();
+comments_template();
+$context->comments_template = ob_get_clean();
+
+echo $context->render($template);
+// echo '<!--';
+// print_r($template);
+// print_r($cntext);
+// echo '-->';
+?>
