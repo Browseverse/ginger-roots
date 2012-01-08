4fe2e4646c5d2f8df747dff1b28882d8976ba7da
Yoav <yoav@gingerlime.com>
2012-01-08 22:23:04 +0100

    Core Mustache-specific class for Roots: Rootache It provides the basic framework for Mustache integration with Roots Currently supports: * i18n via {{#__}} {{/__}} tags * The Loop via {{#the_loop}} {{/the_loop}} tag (replacing while (have_posts() ...) * Some basic context data, particularly generic one used in the loop (e.g. is_singular) * A few lambda functions like {{#get_the_time}}l, F jS, Y{{/get_the_time}} * some basic partials support via {{> template-name }}

--- functions.php
+++ functions.php
@@ -104,4 +104,109 @@ function roots_entry_meta() {
   echo '<p class="byline author vcard">'. __('Written by', 'roots') .' <a href="'. get_author_posts_url(get_the_author_meta('id')) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
 }
 
+require_once('Mustache.php');
+require_once('MustacheLoader.php');
+
+/* Rootache is a Mustache helper class for roots.
+ *
+ * It supports several standard wp methods as well as:
+ *  - translations using {{#__}} {{/__}} tags
+ *  - partials (include other templates) using {{> }} tag
+ */
+class Rootache extends Mustache {
+    function __construct() {
+        parent::__construct();
+        /* auto loading partials */
+        $this->_partials = new MustacheLoader(dirname(__FILE__));
+        $this::init();
+    }
+
+    public function init() {
+        /* init function is used to (re)-initialize the 'standard'
+         * wordpress variables. This is particularly import from within the loop !
+         *
+         * Note that if you override any of those values 
+         * and you use them within the loop, then these values will be
+         * reset back every loop iteration!
+         */
+        global $wp_query;
+        $this->the_ID = get_the_ID();
+        $this->post_class = join( ' ', get_post_class());
+        $this->is_singular = is_singular();
+        $this->is_archive = is_archive();
+        $this->is_search = is_search();
+        $this->is_user_logged_in = is_user_logged_in();
+        $this->the_permalink = get_permalink();
+        $this->the_title = get_the_title();
+        $this->the_content = apply_filters('the_content', get_the_content());
+        $this->the_excerpt= apply_filters('the_excerpt', get_the_excerpt());
+        $this->home_url = home_url();
+        $this->author_posts_url = get_author_posts_url(get_the_author_meta('id'));
+        $this->the_author = get_the_author();
+        $this->more_pages = ($wp_query->max_num_pages > 1);
+        $this->the_tags = get_the_tag_list('', ', ', ''); /* todo: might want to break this down to array of tags and handle in mustache template */
+        $this->bloginfo['name'] = get_bloginfo('name');
+        $this->siteurl = get_option('siteurl');
+        $this->do_have_posts = !in_the_loop() && have_posts(); /* safe way of calling have_posts without altering state (?) */
+
+        /* TODO: these methods should also move to a mustache template / partials */
+        $this->previous_posts_link = get_previous_posts_link( __( '&larr; Older posts', 'roots' ) );
+        $this->next_posts_link = get_next_posts_link( __( 'Newer posts &rarr;', 'roots' ) );
+        $this->page_navigation = wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>', 'echo' => 0 ));
+    }
+    public function __()
+    {
+        return array('Rootache', 'translate');
+    }
+
+    public function _the_loop($str) { 
+        $ret = ""; 
+        while ( have_posts() ) : 
+            the_post(); 
+            //$me = new Rootache; 
+            $this->init();
+            $ret .= $this->render($str); 
+        endwhile; 
+        return $ret; 
+    }
+
+    public static function _get_the_time($str) { return get_the_time($str); }
+
+    public function the_loop() { return array('Rootache', '_the_loop'); }
+    public function get_the_time() { return array('Rootache', '_get_the_time'); }
+
+    /* Translates strings with embedded tags (normally translated using %s placeholders)
+     * e.g. :   {{#__}}Hello World{{/__}}
+     *          {{#__}}Hello {{something}}{{/__}}
+     *          {{#__}} Date: {{#get_the_time}}c{{/get_the_time}} {{/__}}
+     */
+    public function translate($str) {
+        /* Finding sections within the template (e.g. {{#something}}text{{/something}}
+         *  Those need to be constructed and replaced first
+         *  storing matches into $section_matches (so we can swap them back)
+         */
+        $template = $str;
+        $section_matches = array();
+		while ($section = $this->_findSection($template)) {
+            list($before, $type, $tag_name, $content, $after) = $section;
+            $section_text = '{{' . $type . $tag_name . '}}' . $content . '{{/' . $tag_name . '}}';
+            array_push($section_matches, $section_text);
+            $str = __(str_replace($section_text, '%s', $str), 'roots');
+            $template = $after;
+        }
+        // searching for all standard {{tags}} in the string
+        $matches = array();
+        if ( preg_match_all('/{{\s*.*?\s*}}/', $str, &$matches) ) {
+            // first we remove ALL tags and replace with %s and retrieve the translated version
+            $str = __(preg_replace('/{{\s*.*?\s*}}/', '%s', $str), 'roots'); 
+        }
+        // then replace %s back to {{tag}} or sections with the matches
+        if ( $tags = array_merge($matches[0], $section_matches) ) { 
+            return vsprintf($str, $tags);
+        } 
+        else
+            return __($str, 'roots');
+    }
+}
+
 ?>
