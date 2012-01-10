0afeb7e2138c814e8f1a80254fbe2b7e164cdb14
Yoav <yoav@gingerlime.com>
2012-01-10 23:27:21 +0100

    forgot to include this in the last commit :/

--- inc/roots-actions.php
+++ inc/roots-actions.php
@@ -1,10 +1,11 @@
 <?php
 
-add_action('roots_head', 'roots_google_analytics');
-add_action('roots_head', 'roots_fout_b_gone');
-add_action('roots_head', 'roots_1140_head');
-add_action('roots_head', 'roots_adapt_head');
-add_action('roots_head', 'roots_foundation_head');
+/* mustache note: 
+        if you're adding an action purely to output extra stuff to the header/footer
+        you might be better off converting it into a mustache partial.
+        Place it inside the inc/ folder and then call it from any other mustache template
+        using {{> partial_name }}
+*/
 add_action('roots_head', 'roots_bootstrap_head');
 add_action('roots_stylesheets', 'roots_get_stylesheets');
 add_action('roots_header_before', 'roots_1140_header_before');
@@ -17,116 +18,18 @@ add_action('roots_footer_before', 'roots_bootstrap_footer_before');
 add_action('roots_footer_after', 'roots_bootstrap_footer_after');
 add_action('roots_post_inside_before', 'roots_page_breadcrumb');
 
-function roots_google_analytics() {
-  global $roots_options;
-  $roots_google_analytics_id = $roots_options['google_analytics_id'];
-  $get_roots_google_analytics_id = esc_attr($roots_options['google_analytics_id']);
-  if ($roots_google_analytics_id !== '') {
-    echo "\n\t<script>\n";
-    echo "\t\tvar _gaq=[['_setAccount','$get_roots_google_analytics_id'],['_trackPageview'],['_trackPageLoadTime']];\n";
-    echo "\t\t(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];\n";
-    echo "\t\tg.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';\n";
-    echo "\t\ts.parentNode.insertBefore(g,s)}(document,'script'));\n";
-    echo "\t</script>\n";
-  }
-}
-
-function roots_fout_b_gone() {
-  global $roots_options;
-  $roots_fout_b_gone = $roots_options['fout_b_gone'];
-  $template_uri = get_template_directory_uri();
-  if ($roots_fout_b_gone === true) {
-    echo "\t<script src=\"$template_uri/js/libs/foutbgone.min.js\"></script>\n";
-    echo "\t<script>\n";
-    echo "\t\tfbg.hideFOUT('asap', 100);\n";
-    echo "\t</script>";
-  }
-}
-
-function roots_1140_head() {
-  global $roots_options;
-  $roots_css_framework = $roots_options['css_framework'];
-  $template_uri = get_template_directory_uri();
-  if ($roots_css_framework === '1140') {
-    echo "\t<script src=\"$template_uri/js/libs/css3-mediaqueries.js\"></script>";
-  }
-}
-
-function roots_adapt_head() {
-  global $roots_options;
-  $roots_css_framework = $roots_options['css_framework'];
-  $template_uri = get_template_directory_uri();
-  if ($roots_css_framework === 'adapt') {
-    echo "\n\t<script>\n";
-    echo "\t\tvar ADAPT_CONFIG = {\n";
-    echo "\t\t\tpath: '$template_uri/css/adapt/',\n";
-    echo "\t\t\tdynamic: true,\n";
-    echo "\t\t\trange: [\n";
-    echo "\t\t\t\t'0px    to 760px  = mobile.css',\n";
-    echo "\t\t\t\t'760px  to 980px  = 720.css',\n";
-    echo "\t\t\t\t'980px  to 1280px = 960.css',\n";
-    echo "\t\t\t\t'1280px to 1600px = 1200.css',\n";
-    echo "\t\t\t\t'1600px to 1920px = 1560.css',\n";
-    echo "\t\t\t\t'1920px           = fluid.css'\n";
-    echo "\t\t\t]\n";
-    echo "\t\t};\n";
-    echo "\t</script>\n";
-    echo "\t<script src=\"$template_uri/js/libs/adapt.min.js\"></script>";
-  }
-}
-
-function roots_foundation_head() {
-  global $roots_options;
-  $roots_css_framework = $roots_options['css_framework'];
-  $template_uri = get_template_directory_uri();
-  if ($roots_css_framework === 'foundation') {
-	echo "\t<!-- Combine and Compress These Javascript Files -->\n";  
-    echo "\t<script src=\"$template_uri/js/foundation/jquery.reveal.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/foundation/jquery.orbit-1.3.0.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/foundation/forms.jquery.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/foundation/jquery.customforms.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/foundation/jquery.placeholder.min.js\"></script>\n";
-    echo "\t<!--  End Combine and Compress Javascript Files -->\n";
-    echo "\t<script src=\"$template_uri/js/foundation/app.js\"></script>\n";
-    echo "\t<!-- IE Fix for HTML 5 Tags -->\n";
-    echo "\t<!--[if lt IE 9]>\n";
-    echo "\t\t<script src=\"http://html5shiv.googlecode.com/svn/trunk/html5.js\"></script>\n";
-    echo "\t<![endif]-->\n";            
-  }
-}
-
 function roots_bootstrap_head() {
+  /* left out this function just to deal with the mutually exclusive root options
+  /*  (not sure whether this is strictly necessary though) 
+  */
   global $roots_options;
-  $roots_css_framework = $roots_options['css_framework'];
   $roots_bootstrap_js = $roots_options['bootstrap_javascript'];
   $roots_bootstrap_less_js = $roots_options['bootstrap_less_javascript'];  
-  $template_uri = get_template_directory_uri();
-  if ($roots_css_framework === 'bootstrap_less') {
-    echo "\t<script src=\"$template_uri/js/bootstrap/less-1.1.3.min.js\"></script>\n";     
-  }  
   if ($roots_bootstrap_js === true) {
   	$roots_options['bootstrap_less_javascript'] = false;
-  	
-  	echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-modal.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-alerts.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-twipsy.js\"></script>\n"; 
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-popover.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-dropdown.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-scrollspy.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-tabs.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-buttons.js\"></script>\n";                           
   }
   if ($roots_bootstrap_less_js === true) {
   	$roots_options['bootstrap_javascript'] = false;
-  	
-  	echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-modal.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-alerts.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-twipsy.js\"></script>\n"; 
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-popover.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-dropdown.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-scrollspy.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-tabs.js\"></script>\n";
-    echo "\t<script src=\"$template_uri/js/bootstrap/bootstrap-buttons.js\"></script>\n";                             
   }  
 }
 
