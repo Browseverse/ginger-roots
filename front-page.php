4c8e96820aab37ae0c6dc89d19bd7cd98c6eff83
Yoav <yoav@gingerlime.com>
2012-01-13 14:52:01 +0100

    converted page, page-custom, page-full and front-page to mustache

--- front-page.php
+++ front-page.php
@@ -1,24 +1,18 @@
-<?php get_header(); ?>
-  <?php roots_content_before(); ?>
-    <div id="content" class="<?php echo $roots_options['container_class']; ?>">
-    <?php roots_main_before(); ?>
-      <div id="main" class="<?php echo $roots_options['main_class']; ?>" role="main">
-        <div class="container">
-          <?php roots_loop_before(); ?>
-          <?php get_template_part('loop', 'page'); ?>
-          <?php roots_loop_after(); ?>
-        </div>
-      </div><!-- /#main -->
-    <?php roots_main_after(); ?>
-    <?php roots_sidebar_before(); ?>
-      <aside id="sidebar" class="<?php echo $roots_options['sidebar_class']; ?>" role="complementary">
-      <?php roots_sidebar_inside_before(); ?>
-        <div class="container">
-          <?php get_sidebar(); ?>
-        </div>
-      <?php roots_sidebar_inside_after(); ?>
-      </aside><!-- /#sidebar -->
-    <?php roots_sidebar_after(); ?>
-    </div><!-- /#content -->
-  <?php roots_content_after(); ?>
-<?php get_footer(); ?>
+<?php 
+get_header();
+global $context;
+$template = $context->update(__FILE__);
+
+/* getting loop-index */
+ob_start();
+get_template_part('loop', 'page');
+$context->loop_page = ob_get_clean();
+/* getting sidebar */
+ob_start();
+get_sidebar();
+$context->sidebar = ob_get_clean();
+
+echo $context->render($template);
+
+get_footer(); 
+?>
